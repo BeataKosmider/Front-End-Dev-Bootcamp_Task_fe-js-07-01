6c16f3c88b29971f13b1d822d5ab98470a6a9fc8
Beata Kosmider <kosmibe@gmail.com>
2023-03-02 13:37:33 +0100

    task done without comment

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const axios = require("axios");
 const getKittenByAxios = () => {
   axios.get("https://api.thecatapi.com/v1/images/search");
-  /*.then((response) => {
-      return response;
-    })
-    .catch((err) => {
-      return err;
-    })*/
 };
 module.exports = {
   getKittenByAxios,
